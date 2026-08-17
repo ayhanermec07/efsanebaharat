820647804d3f0ca167c49b67dfcf327a38dbf43d
Retrosero <141685548+Retrosero@users.noreply.github.com>
2026-08-17 20:38:39 +0300

    Sayfa gecislerinde mobil kaydirma konumunu sifirla

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
 
 export default function Layout() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [location.pathname, location.search])
+
   return (
     <div className="shop-shell flex min-h-screen min-w-0 flex-col overflow-x-clip">
       <Header />
